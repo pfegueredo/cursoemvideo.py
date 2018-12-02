2917ead64cc8a3ad040bf2ba9953520dbdf91bdf
pfegueredo <paulo.fegueredo@gmail.com>
2018-12-02 16:39:54 -0300

    Add ex002

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Escrever uma tela com interação com o usuário, onde pergunta qual seu nome e responde a ele, prazer em conhece-lo.
+
+nome = input(print('Digite seu nome: '))
+
+print('É um prazer te conhecer, ', nome)
