871b839883dae7794ceb1d1665283ab1c0c32fff
Paulo Fegueredo <paulo.fegueredo@gmail.com>
2018-12-02 22:39:23 -0300

    add ex003

---
+++ ex003.py
@@ -0,0 +1,8 @@
+#Soma de dois valores
+
+
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite mais um valor: '))
+soma = valor1 + valor2
+print('A soma de {0} e {1} é {2}'.format(valor1, valor2, soma))
+ 
