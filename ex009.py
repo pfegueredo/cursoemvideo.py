7e7323e9059f2e731a3f44ebf834cf4ec0d9a82d
Paulo Fegueredo <paulo.fegueredo@gmail.com>
2018-12-05 18:27:59 -0300

    add ex009

---
+++ ex009.py
@@ -0,0 +1,7 @@
+#Exercicio/Desafio 009
+#Faça um programa que leia um número e mostre na tela a sua tabuada
+n1 = int(input('Digite um número: '))
+x=1
+while x<=10:  
+  print(n1,' x 1 = ', n1*x)
+  x=x+1
