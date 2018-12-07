abd0d2eaf96ee2e4fb7a4454adf691aa7745d063
Paulo Fegueredo <paulo.fegueredo@gmail.com>
2018-12-07 15:14:19 -0300

    Add ex011

--- ex011.py
+++ ex011.py
@@ -9,4 +9,3 @@ tinta = area / 2
 print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(largura, altura, area))
 
 print('Para pintar essa parede, você precisará de {} litros de tinta.'.format(tinta))
-
